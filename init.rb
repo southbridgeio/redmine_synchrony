261947d2c56147df97136585734aaf4c39793d53
kanfet <byte.developer@gmail.com>
2013-09-10 19:46:23 +0600

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_synchrony do
+  name 'Redmine Synchrony plugin'
+  author 'Pavel Nemkin'
+  description 'Plugin synchronize issues via API'
+  version '0.0.1'
+  url 'https://github.com/kanfet/redmine_synchrony'
+  author_url 'https://github.com/kanfet'
+end
