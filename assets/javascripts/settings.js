c1d23266dcd34571e3e784588329ccc6a2c45485
kanfet <byte.developer@gmail.com>
2013-09-12 00:24:17 +0600

    plugin settings

--- assets/javascripts/settings.js
+++ assets/javascripts/settings.js
@@ -1,5 +1,37 @@
 $(function(){
 
+  var properties = $('#synchrony-sites')
+  var translations = properties.data('i18n')
+  var projects = properties.data('projects')
+  var trackers = properties.data('trackers')
+
+  var label = function(field_name, nextId){
+    return $('<label for="settings_redmine_' + nextId + '_' + field_name + '">' + translations[field_name] + '</label>');
+  };
+
+  var inputField = function(field_name, nextId){
+    return $('<p>').append(
+      label(field_name, nextId)
+    ).append(
+      $('<input type="text" size="60" id="settings_redmine_' + nextId + '_' + field_name +
+        '" name="settings[redmine][][' + field_name + ']">')
+    );
+  };
+
+  var selectField = function(field_name, values, nextId){
+    var options = [$('<option></option>')];
+    for (var id in values) {
+      if (values.hasOwnProperty(id)) {
+        options.push($('<option value="' + id + '">' + values[id] + '</option>'))
+      }
+    }
+    return $('<p>').append(
+      label(field_name, nextId)
+    ).append(
+      $('<select id="settings_redmine_' + nextId +'_' + field_name + '" name="settings[redmine][][' + field_name + ']">').append(options)
+    );
+  };
+
   $('.add-synchrony-site').click(function(event){
     event.preventDefault();
     var nextRedmine = $('.synchrony-site-settings').length;
@@ -7,11 +39,15 @@ $(function(){
       $('<fieldset>', { class: 'box synchrony-site-settings' }).append(
           $('<a href="#" class="icon icon-del contextual delete-synchrony-site">Delete</a>')
         ).append(
-          $('<p>').append(
-              $('<label for="settings_redmine_' + nextRedmine + '_source_site">Source site</label>')
-            ).append(
-              $('<input type="text" id="settings_redmine_' + nextRedmine + '_source_site" name="settings[redmine][][source_site]">')
-            )
+          inputField('source_site', nextRedmine)
+        ).append(
+          inputField('api_key', nextRedmine)
+        ).append(
+          inputField('source_tracker', nextRedmine)
+        ).append(
+          selectField('target_project', projects, nextRedmine)
+        ).append(
+          selectField('target_tracker', trackers, nextRedmine)
         )
     );
   });
