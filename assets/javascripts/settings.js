28d25d676eadee2c4c4d049568c22421fd52d373
kanfet <byte.developer@gmail.com>
2013-09-14 19:16:35 +0600

    client(in browser) validation for plugin settings

--- assets/javascripts/settings.js
+++ assets/javascripts/settings.js
@@ -1,6 +1,4 @@
 $(function(){
-  // TODO presence of params validation before save
-  // TODO add unique project validation before save
   var properties = $('#synchrony-sites')
   var translations = properties.data('i18n')
   var projects = properties.data('projects')
@@ -34,6 +32,62 @@ $(function(){
     );
   };
 
+  var validatePresence = function() {
+    var errorCount = 0;
+    $('#settings input, #settings select').not('#settings input[type="hidden"]').each(function(){
+      var $this = $(this);
+      if($this.val() === ''){
+        if(!errorDisplayed($this)) {
+          displayError($this, translations['errors.blank']);
+        }
+        errorCount++;
+      } else {
+        removeError($this);
+      }
+    });
+    return !(errorCount > 0);
+  };
+
+  var validateProjectUniqueness = function() {
+    var errorCount = 0;
+    var projects = [];
+    $('select[name="settings[redmine][][target_project]"]').each(function(){
+      var $this = $(this);
+      var project = $this.val();
+      var existProjectIndex = $.inArray(project, projects);
+      if(existProjectIndex >= 0) {
+        errorCount++;
+        if(!errorDisplayed($this)) {
+          displayError($this, translations['errors.uniqueness']);
+        }
+        var existProjectSelect = $($('select[name="settings[redmine][][target_project]"]')[existProjectIndex]);
+        if(!errorDisplayed(existProjectSelect)) {
+          displayError(existProjectSelect, translations['errors.uniqueness']);
+        }
+      } else {
+        removeError($this);
+      }
+      projects.push(project);
+    });
+    return !(errorCount > 0);
+  };
+
+  var errorDisplayed = function(element) {
+    return element.closest('p').find('.error-notice').length > 0;
+  };
+
+  var displayError = function(element, message){
+    element.addClass('error-setting');
+    element.closest('p').append(
+      $('<span class="error-notice">' + message + '</span>')
+    );
+  };
+
+  var removeError = function(element){
+    element.removeClass('error-setting');
+    element.closest('p').find('.error-notice').remove();
+  };
+
   $('.add-synchrony-site').click(function(event){
     event.preventDefault();
     var nextRedmine = $('.synchrony-site-settings').length;
@@ -57,6 +111,15 @@ $(function(){
   $('.delete-synchrony-site').live('click', function(event){
     event.preventDefault();
     $(this).closest('.synchrony-site-settings').remove();
+  });
+
+  $('#settings form').submit(function(event){
+    if(validatePresence() && validateProjectUniqueness()) {
+      $(this).off('submit');
+      $(this).trigger('submit');
+    } else {
+      event.preventDefault();
+    }
   });
 
 });
