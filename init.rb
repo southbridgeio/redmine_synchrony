3123c87e032ae2644645714bb54ddd90c53c86ea
Igor Olemskoi <igor@southbridge.ru>
2016-02-02 16:53:58 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,10 +2,10 @@ require 'active_resource'
 
 Redmine::Plugin.register :redmine_synchrony do
   name 'Redmine Synchrony plugin'
-  author 'Pavel Nemkin'
+  author 'Centos-admin.ru'
   description 'Plugin makes copies of issues and journals from another redmine instance via API.'
   version '0.0.5'
-  url 'https://github.com/olemskoi/redmine_synchrony'
-  author_url 'https://github.com/kanfet'
+  url 'https://github.com/centosadmin/redmine_synchrony'
+  author_url 'http://centos-admin.ru'
   settings default: {'empty' => true}, partial: 'settings/synchrony_settings'
 end
