07db703db7eb2768ab959832e26c5ca674021e4d
Igor Olemskoi <igor@southbridge.ru>
2019-11-19 23:09:15 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -4,10 +4,10 @@ Rails.application.config.eager_load_paths << "#{Rails.root}/plugins/redmine_sync
 
 Redmine::Plugin.register :redmine_synchrony do
   name 'Redmine Synchrony plugin'
-  author 'Centos-admin.ru'
+  author 'Southbridge'
   description 'Plugin makes copies of issues and journals from another redmine instance via API.'
   version '0.0.5'
-  url 'https://github.com/centosadmin/redmine_synchrony'
-  author_url 'http://centos-admin.ru'
+  url 'https://github.com/southbridgeio/redmine_synchrony'
+  author_url 'https://southbridge.io'
   settings default: {'empty' => true}, partial: 'settings/synchrony_settings'
 end
